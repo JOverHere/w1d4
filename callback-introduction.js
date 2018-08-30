7afc0bf221d30d8dc5e5290d238957c6265c74e5
Jenna Woodley <jenatalie@msn.com>
2018-08-30 17:33:18 +0000

    for each waldo

--- callback-introduction.js
+++ callback-introduction.js
@@ -1,23 +1,18 @@
+
+
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  // newArray is scoped within findWaldo
-  //var newArray =[];
-  // later: trying using an array method
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      // 1 - call the function
-      // 2 - pushing the result of the function inside of newArray
-      //newArray.push(found(arr[i], i))
-      found(i);  // execute callback
+  arr.forEach(function callback(name, index) {
+    if (name === "Waldo") {
+      found(index);
     }
-  }
-
+  })
 }
 
 function actionWhenFound(index) {
 
-  console.log(`Found Waldo at ${index}`);
+  console.log(`Found Waldo at index ${index}`);
 }
+
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 // console.log(actionWhenFound());
-
