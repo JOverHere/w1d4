b3ba6441b6dffe3a9f3ba2f0bcc1571299f8c05f
Jenna Woodley <jenatalie@msn.com>
2018-08-30 17:32:15 +0000

    for each waldo

---
+++ callbackForEach.js
@@ -0,0 +1,16 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(function callback(name, index) {
+    if (name === "Waldo") {
+      found(index);
+    }
+  })
+}
+
+function actionWhenFound(index) {
+
+  console.log(`Found Waldo at index ${index}`);
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+// console.log(actionWhenFound());
